067423baf961596330ce702bded112eba0c40f1f
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2025-04-07 01:06:03 +0530

    Time: 514 ms (8.73%), Space: 8.9 MB (29.41%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+   bool valid(char c,vector<vector<char>>&board,int row,int col)
+   {
+    for(int i=0;i<9;i++)
+    {
+        if(board[i][col]==c)
+        return false;
+
+        if(board[row][i]==c)
+        return false;
+
+        if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+        return false;
+    }
+    return true;
+   }
+
+   bool sudo(vector<vector<char>>& board)
+   {
+
+    for(int i=0;i<board.size();i++)
+    {
+        for(int j=0;j<board[0].size();j++) 
+        {
+            if(board[i][j]=='.')
+            {
+                for(char c='1';c<='9';c++)
+                {
+                    if(valid( c,board,i,j))
+                    {
+                        board[i][j]=c;
+                    
+
+                    if(sudo(board)==true)
+                    return true;
+                   else
+                   board[i][j]='.';
+                    }
+                 
+                
+
+                }
+                return false;
+
+            }
+        }
+
+    }
+    return true;
+   }
+    void solveSudoku(vector<vector<char>>& board) {
+
+        sudo(board);
+
+
+        
+    }
+};
