77f157157d8b2712d030abda4bfc6581024fa949
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2025-07-18 19:48:44 +0530

    Time: 0 ms (100%), Space: 22.5 MB (96.71%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
+        //two pointer approach of finding dupilcate
+        //first we have to pointer at 0 imagine it is unique and search by pointer j
+
         int i=0;
-        for(int j=0;j<nums.size();j++)
+        for(int j=1;j<nums.size();j++)
         {
-            if(nums[i]!=nums[j])
-            i++;
-            nums[i]=nums[j];
+            if(nums[j]!=nums[i])
+            {
+                i++;
+                nums[i]=nums[j];
+
+            }
         }
-       return i+1; 
+
+     return (i+1); 
     }
 };
