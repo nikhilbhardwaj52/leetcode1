91b278d6dadc5f79876b86bf5600fde6338a19e9
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2025-09-01 00:46:40 +0530

    Time: 0 ms (100%), Space: 8.7 MB (11.54%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -1,35 +1,11 @@
 class Solution {
 public:
-      int sum1(int n)
-      {
-        int sumodd=1;
-        int sumod=1;
-         for(int i=1;i<n;i++)
-         {
-            sumodd+=2;
-            sumod+=sumodd;
-
-         }
-         return sumod;
-      }
-      int sum2(int n)
-      {
-        int sumeven=2;
-        int sumev=2;
-        for(int i=1;i<n;i++)
-        {
-            sumeven+=2;
-            sumev+=sumeven;
-        }
-        return sumev;
-      }
-
     int gcdOfOddEvenSums(int n) {
-    
 
-       int a=sum1(n);
-      int b=sum2(n);
-        
+        int a=n*n;
+        int b=n*(n+1);
+
         return gcd(a,b);
+        
     }
 };
