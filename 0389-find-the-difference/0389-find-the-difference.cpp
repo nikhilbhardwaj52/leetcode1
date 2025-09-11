b1e8130fb1ac9d291d40cd09eea62de685f66bf4
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2025-09-12 03:47:21 +0530

    Time: 0 ms (100%), Space: 9.2 MB (52.54%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
 
-        unordered_map<char,int>m;
+        vector<int>m(26,0);
         for(int i=0;i<s.size();i++)
         {
-            m[s[i]]++;
+            m[s[i]-'a']++;
         }
         for(int j=0;j<t.size();j++)
         {
-            if(m[t[j]]>0)
+            if(m[t[j]-'a']>0)
             {
-                m[t[j]]--;
+                m[t[j]-'a']--;
             }
             else
             {
