b20781f236ec1794c5f2582ce426879c08b02340
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2025-10-02 02:52:14 +0530

    Time: 3 ms (40.77%), Space: 9.2 MB (85.56%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.cpp
+++ 2886-faulty-keyboard/2886-faulty-keyboard.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-   string final(string s2)
-   {
-       int i=0;
-       int j=s2.size()-1;
-       while(i<j)
-       {
-         swap(s2[i],s2[j]);
-         i++;
-         j--;
-       }
-     return s2;
-   }
-
     string finalString(string s) {
         string s1="";
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='i')
             {
-               s1=final(s1);
+                reverse(s1.begin(),s1.end());
             }
-            else{
-           s1+=s[i]; 
-            } 
+            else
+            {
+                s1.push_back(s[i]);
+            }
         }
-    
-     return s1;
+       return s1; 
     }
 };
