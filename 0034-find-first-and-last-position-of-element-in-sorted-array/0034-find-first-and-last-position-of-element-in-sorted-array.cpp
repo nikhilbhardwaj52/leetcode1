e4acba9abe3441c91ae8865c4fec24fc1e1afa61
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2025-12-30 21:54:10 +0530

    Time: 0 ms (100%), Space: 17.4 MB (85.49%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,73 +1,60 @@
 class Solution {
 public:
-
-     int first(vector<int>nums,int target)
-     {
-        
-         int start=0;
-         int end=nums.size()-1;
-         int ans=-1;
-
-         while(start<=end)
-         {
-        int mid=end+(start-end)/2;
-
-        if(nums[mid]==target)
-        {
-           ans=mid;
-           end=mid-1;
-        }
-        else if(nums[mid]<target)
-        {
-            start=mid+1;
-        }
-        else
-        {
-            end=mid-1;
-        }
-         }
-       
-       return ans;
-     }
-
-
-
-     int second(vector<int>nums,int target)
-     {
-        int start=0;
-        int end=nums.size()-1;
-        int ans =-1;
-
-        while(start<=end)
-        {
-            int mid=end+(start-end)/2;
-
-            if(nums[mid]==target)
-            {
-                ans=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]<target)
-            {
-                start=mid+1;
-            }
-            else
-            {
-                end=mid-1;
-            }
-         
-          
-        }
-        return ans;
-     }
     vector<int> searchRange(vector<int>& nums, int target) {
+           
+           int n=nums.size();
 
 
-       int f=first(nums,target);
-       int s=second(nums,target);
+           int start=0;
+           int end=n-1;
+           int first=-1;
 
-       return nums.empty()?vector<int>{-1,-1}:vector<int>{f,s};
-       
-       
+           while(start<=end)
+           {
+              
+              int mid= start+(end-start)/2;
+
+              if(nums[mid]==target)
+              {
+               first=mid;
+               end=mid-1;
+              }
+
+              else if(nums[mid]<target)
+              start=mid+1;
+
+              else
+               end=mid-1;
+
+           }
+
+              start=0;
+              end=n-1;
+           int second=-1;
+
+           while(start<=end)
+           {
+             
+               int mid=start+(end-start)/2;
+
+               if(nums[mid]==target)
+                {
+                   second=mid;
+                   start=mid+1;
+                }
+                else if(nums[mid]<target)
+                start=mid+1;
+
+                else
+                end=mid-1;
+           }
+           
+
+         vector<int>ans;
+         ans.push_back(first);
+         ans.push_back(second);    
+
+
+     return ans; 
     }
 };
