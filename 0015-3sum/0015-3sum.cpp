d791bfd9150c1fd0eaef967398628fbe556f7d93
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2026-01-29 00:38:46 +0530

    Time: 1140 ms (10.02%), Space: 293.5 MB (8.27%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,51 +1,45 @@
-
-
-
-        class Solution {
+class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        //sort the array
+        sort(nums.begin(),nums.end());
 
-        // Approach 2 - Modified 2 sum
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
+        //vector
+        set<vector<int>>vt;
+        vector<vector<int>>v;
 
-        for (int i = 0; i < n; i++) {
+        //new 
 
-            // skip the first element
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+         for(int i=0;i<nums.size();i++)
+         {
+            int first=0-nums[i];
 
-            int first = nums[i];
-            int right = n - 1;
-            int left = i + 1;
+            int start=i+1;
+            int end=nums.size()-1;
 
-            while (left < right) {
-
-                // element found
-                if (first + nums[left] + nums[right] == 0) {
-                    ans.push_back({first, nums[left], nums[right]});
-
-                    // skip duplicates
-                    while (left < right &&
-                           nums[left] == nums[left + 1]) { // req base case
-                        left++;
-                    }
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-                    left++;
-                    right--;
-
-                } else if (first + nums[left] + nums[right] > 0) {
-                    right--;
-                } else {
-                    left++;
+            while(start<end)
+            {
+                if(nums[start]+nums[end]==first)
+                {
+                vt.insert({nums[start],nums[end],nums[i]});
+                start++;
+                end--;
                 }
-            }
-        }
 
-        return ans;
+                else if(nums[start]+nums[end]>first)
+                end--;
+
+                else
+                start++;
+            }
+         }
+
+         for(auto it=vt.begin();it!=vt.end();it++ )
+         {
+            v.push_back(*it);
+         }
         
+
+        return v;
     }
 };
