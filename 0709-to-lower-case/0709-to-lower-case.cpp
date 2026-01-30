09a42f692c2484311c9974b1d68fcdce7bed1d53
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2026-01-31 02:00:15 +0530

    Time: 0 ms (100%), Space: 8.2 MB (53.81%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]>='A'&&s[i]<='Z')
+            {
+                s[i]=s[i]+32;
+            }
+        }
+
+        return s;
+        
+    }
+};
