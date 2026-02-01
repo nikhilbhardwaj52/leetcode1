24d370c1838646a18e555ff8bee6f773fb93e9a7
Nikhil bhardwaj <154703392+nikhilbhardwaj52@users.noreply.github.com>
2026-02-01 22:50:28 +0530

    Time: 0 ms (100%), Space: 9 MB (94.58%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-
-     string add(string num1,string num2)
-     {
-        int index1=num1.size()-1,index2=num2.size()-1;
-        int carry=0,sum=0;
-        string ans;
-
-        while(index2>=0)
-        {
-            sum= (num1[index1]-'0')+(num2[index2]-'0')+carry;
-            carry=sum/10;
-            char c=sum%10+'0';
-            ans+=c;
-            index2--,index1--;
-        }
-     
-       while(index1>=0)
-     {
-        sum=(num1[index1]-'0')+carry;
-        carry=sum/10;
-        char c=sum%10+'0';
-        ans+=c;
-        index1--;
-     }
-     
-
-     if(carry)
-     ans+='1';
-     reverse(ans.begin(),ans.end());
-     return ans;
-     }
     string addStrings(string num1, string num2) {
 
-       if(num1.size()<num2.size())
-       return add(num2,num1);
+        int endfirst=num1.size()-1;
+        int endsec=num2.size()-1;
+        string s="";
+        int carry=0;
+        int sum=0; 
 
-       else
-       return add(num1,num2);
-        
+        while(endfirst>=0||endsec>=0||carry)
+        {
+            
+            if(endfirst>=0)
+            {
+                sum+=num1[endfirst]-'0';
+                endfirst--; 
+            }
+            if(endsec>=0)
+            {               
+                 sum+=num2[endsec]-'0';
+                endsec--;
+            }
+          
+          s.push_back(sum%10+'0');
+
+          carry=sum/10;
+
+          sum=carry;
+          
+          
+        }
+        reverse(s.begin(),s.end());
+
+    return s;
     }
 };
